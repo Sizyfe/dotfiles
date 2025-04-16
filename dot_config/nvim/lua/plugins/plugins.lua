db4ffc532987a9f91497b5c9814402447bcd99a8
Sizyff <182140235+Sizyff@users.noreply.github.com>
2025-04-16 20:48:15 +0200

    🎉 Initial commit

---
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        virtual_text = false,
+      },
+      servers = {
+        html = {
+          filetypes = { "html", "templ", "twig" },
+        },
+      },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          never_show = { ".git" },
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = "all",
+    },
+  },
+}
